8ca268cd882329858c9c82d1e0c0f17b723ac859
k1nderko <107281239+k1nderko@users.noreply.github.com>
2023-10-18 12:28:55 +0300

    Create task-02.js

---
+++ js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const ul = document.querySelector('#ingredients');
+
+const ingredientListItems = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  return li;
+});
+
+ul.append(...ingredientListItems);
