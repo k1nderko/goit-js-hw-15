786cbb46131395b3930a741c25ab2e9d69b00977
k1nderko <107281239+k1nderko@users.noreply.github.com>
2023-10-18 17:01:49 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,13 @@
-const categoriesList = document.querySelector('#categories');
-const categoriesItems = categoriesList.querySelectorAll('.item');
+const list = document.querySelector("#categories");
+const listItems = list.children;
+const numberOfCategories = listItems.length;
 
-console.log(`Number of categories: ${categoriesItems.length}`);
+console.log(`Number of categories: ${numberOfCategories}`);
 
-categoriesItems.forEach(item => {
-  const categoryTitle = item.querySelector('h2').textContent;
-  const categoryElements = item.querySelectorAll('ul li').length;
+Array.from(listItems).forEach((item) => {
+  const categoryTitle = item.firstElementChild.textContent;
+  const categoryItemsCount = item.lastElementChild.children.length;
 
   console.log(`Category: ${categoryTitle}`);
-  console.log(`Elements: ${categoryElements}`);
+  console.log(`Elements: ${categoryItemsCount}`);
 });
